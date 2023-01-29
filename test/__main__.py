f2f525b5479195bd4e95ea60afe6f3a1220e0c4c
Liam Pribis <jackpribis@gmail.com>
2023-01-29 19:40:20 +1300

    Fix src_inc_script tests

--- test/__main__.py
+++ test/__main__.py
@@ -1,9 +1,9 @@
 import test.single_artifact
 import test.out_dir
 import test.tags
-import test.integration
+import test.src_inc_script
 
 test.single_artifact.run()
 test.out_dir.run()
 test.tags.run()
-test.integration.run()
+test.src_inc_script.run()
--- test/src_inc_script/__init__.py
+++ test/src_inc_script/__init__.py
@@ -4,18 +4,19 @@ from .. import util
 def testcase_build_main1():
    util.shell("rm -rf ./out")
    util.shell("../../loafer -l main1")
-   util.command_expect_output("./out/main1/main1", "", 20)
+   util.command_expect_output("out/main1/main1", "", 40)
 
 def testcase_build_main2():
    util.shell("rm -rf ./out")
    util.shell("../../loafer -l main2")
-   util.command_expect_output("./out/main2/main2", "", 40)
+   util.command_expect_output("out/main2/main2", "", 20)
 
 def testcase_script_with_requires():
    util.shell("rm -rf ./out")
-   util.command_expect_output("../../loafer prereq_main1_add1", "41")
+   util.command_expect_output("../../loafer prereq_main1_add1", "41\n")
 
 def run():
+   util.cd(Path(__file__).parent)
    testcase_build_main1()
    testcase_build_main2()
    testcase_script_with_requires()
